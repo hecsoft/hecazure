05c374c614e959eb2fb900843c69dc2a69314ea7
hecsoft <contact@hecsoft.net>
2017-01-30 22:00:00 +0100

    Package Version 0.0.1   plg_authentication_azure version 0.0.1     + Some language string added   plg_system_azure version 0.0.1     + Add french language     + Manage Azure return URL

--- packages/plg_system_azure/azure.php
+++ packages/plg_system_azure/azure.php
@@ -2,7 +2,7 @@
 /**
  * @package     Azure Joomla Authentication
  * @subpackage  System.azure
- * @version 	1.0.0
+ * @version 	0.0.1
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -13,7 +13,7 @@ jimport('joomla.log.log');
 /**
  * Azure Authentication Plugin
  *
- * @since  1.0
+ * @since  0.1
  */
 
 class PlgSystemAzure extends JPlugin
@@ -25,14 +25,14 @@ class PlgSystemAzure extends JPlugin
 	 * @since  1.0
 	 */
 	protected $app;
-
+	private $session=null;
 	/**
 	 * Azure Authentication run on onAfterInitialise
-	 * Only purpose is to check if we come from azure and if we have code
+	 * Only purpose is to check if we come from azure and if we have code and call login function
 	 *
 	 * @return  void
 	 *
-	 * @since   1.0
+	 * @since   0.1
 	 * @throws  InvalidArgumentException
 	 */
 	public function onAfterInitialise()
@@ -55,11 +55,41 @@ class PlgSystemAzure extends JPlugin
 			if (strlen($code) && strlen($state)) 
 			{
 				$app->login(array("username"=>"", "password"=>"none"));
+				
+				$session=$this->getSession();
+				$return = $session->get("azure_return","");
+					
+				$session->set("azure_return","");
+				if (strlen($return))
+					$app->redirect($return);
+				else
+					$app->redirect(JUri::base(false));
 				return;
 				
 			}
 		}
 	}
-
+	/**
+	 * Get current session (Joomla 2.5 and 3.x are different)
+	 *
+	 * @return  JSession object
+	 *
+	 * @since   0.1
+	 */
+	private function getSession()
+	{
+		if ($this->session==null)
+		{
+			if(version_compare(JVERSION,'3.0.0','<')){
+				// Joomla 2.5
+				$this->session=JFactory::getSession();
+			}
+			else {
+				// Joomla > 2.5
+				$this->session=JFactory::getApplication()->getSession();
+			}
+		}
+		return $this->session;
+	}
 	
 }
